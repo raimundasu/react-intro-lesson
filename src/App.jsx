629c841b0f884e26cc4d9432d69663bab54e991c
raimundasu <raimundas.urbonas1@gmail.com>
2024-03-21 20:00:39 +0200

    create and use custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <>
       <h1>Welcome to react!</h1>
       <button>My first react button</button>
+      <Button />
     </>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>My first custom react button</button>;
+};
+
+export default Button;
