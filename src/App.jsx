dc0a95387bcf0847c8d41c9901a5dcaf08bd85bf
raimundasu <raimundas.urbonas1@gmail.com>
2024-03-21 20:54:17 +0200

    V1_last

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from "./components/Button";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const onBlurEvent = (e) => {
-    console.log(e.target.value);
-  };
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => setCount(count + 1);
+  const handleSubtract = () => setCount(count - 1);
 
   return (
     <div className="container">
@@ -14,9 +16,20 @@ function App() {
         className="avatar"
       />
       <h1>Welcome to react!</h1>
-      <Button text="Add" />
-      <Button text="Subtrack" />
-      <input onBlur={onBlurEvent} />
+      <Button text="Add" onClicking={handleAdd} />
+      <Button text="Subtract" onClicking={handleSubtract} />
+      <input
+        value={count}
+        style={{
+          width: "290px",
+          height: "60px",
+          background: "bisque",
+          border: "2px solid black",
+          borderRadius: "5px",
+          fontSize: "30px",
+          textAlign: "center",
+        }}
+      />
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
   return (
-    <button className="custom-button" onClick={handleClick}>
-      {props.text} {count}
+    <button className="custom-button" onClick={props.onClicking}>
+      {props.text}
     </button>
   );
 };
